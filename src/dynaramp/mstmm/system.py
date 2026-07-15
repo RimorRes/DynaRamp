Fixed static loading (added a `-` sign) :bug:

src/dynaramp/mstmm/system.py
@@ -588,7 +588,7 @@ class MBS:
         # ... and move columns corresponding to known non-zero boundary conditions into a load vector
         u_nz = u_all[:, nonzero_mask]
         z_nz = z_rem[nonzero_mask].astype(np.float64)
-        f = u_nz @ z_nz
+        f = - u_nz @ z_nz
         # Remove the trivial 13th row
         triv_mask = np.ones_like(f, dtype=bool)
         triv_mask[12] = False

tests/mstmm/test_mass_spring.py
@@ -372,13 +372,12 @@ def test_closed_loop_auto_cut():
 
 def test_static_loading():
     k = 20
-    m = 2
+    m = 5
 
     system = create_static_loading()
     u, f, z_rem, rem_bounds = system.overall_transfer(0)
-    print(f)
     z_red = solve(u, f)
 
     state_vecs = system.propagate_state(0, z_red, z_rem, rem_bounds)
-    for sv in state_vecs:
-        print(sv, state_vecs[sv])
+    tip = next(iter(system.tips))
+    assert np.isclose(state_vecs[tip][2], -9.81*m/k, rtol=1e-3)